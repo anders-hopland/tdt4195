f458b4438e1b55af369b8b1150a765ecf9dc3094
anders <andershopl@hotmail.com>
2018-11-08 15:04:43 +0100

    Add project

--- project/program.py
+++ project/program.py
@@ -1,27 +1,46 @@
-import tensorflow as tf  # deep learning library. Tensors are just multi-dimensional arrays
+import tensorflow as tf
 
-mnist = tf.keras.datasets.mnist  # mnist is a dataset of 28x28 images of handwritten digits and their labels
-(x_train, y_train),(x_test, y_test) = mnist.load_data()  # unpacks images to x_train/x_test and labels to y_train/y_test
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = tf.keras.utils.normalize(x_train, axis=1)  # scales data between 0 and 1
-x_test = tf.keras.utils.normalize(x_test, axis=1)  # scales data between 0 and 1
+# Normalize the training data
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-model = tf.keras.models.Sequential()  # a basic feed-forward model
-model.add(tf.keras.layers.Flatten())  # takes our 28x28 and makes it 1x784
-#model.add(tf.keras.layers.Dense(16, activation=tf.nn.relu))  # a simple fully-connected layer, 128 units, relu activation
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))  # a simple fully-connected layer, 128 units, relu activation
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))  # a simple fully-connected layer, 128 units, relu activation
-#model.add(tf.keras.layers.Dense(256, activation=tf.nn.relu))  # a simple fully-connected layer, 128 units, relu activation
-model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))  # our output layer. 10 units for 10 classes. Softmax for probability distribution
+# Model definitions
+def model_1(model):
+    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
 
-model.compile(optimizer='adam',  # Good default optimizer to start with
-              loss='sparse_categorical_crossentropy',  # how will we calculate our "error." Neural network aims to minimize loss.
-              metrics=['accuracy'])  # what to track
 
-model.fit(x_train, y_train, epochs=4)  # train the model
+def model_2(model):
+    model.add(tf.keras.layers.Dense(12, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
 
-val_loss, val_acc = model.evaluate(x_test, y_test)  # evaluate the out of sample data with model
-print(val_loss)  # model's loss (error)
-print(val_acc)  # model's accuracy
 
-#model.save("first_model.model")
+def model_3(model):
+    model.add(tf.keras.layers.Dense(512, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(512, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+
+
+# Test a given model and print the loss and accuracy
+def test_model(model_spec, epochs=4):
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Flatten())
+    model_spec(model)
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=epochs)  # train the model
+
+    val_loss, val_acc = model.evaluate(x_test, y_test)  # evaluate the model using the test data
+    print("Loss: %.4f, accuracy: %.4f" % (val_loss, val_acc))
+
+
+#print("Model 1:")
+#test_model(model_1, epochs=4)
+
+print("Model 3:")
+test_model(model_3, epochs=20)
